5718d19505f28cdd09b5c73e2fef01b785654372
kikairoya <kikairoya@gmail.com>
2010-10-31 10:26:00 +0900

    change definition of uintptr_t for compatibility

--- ntfmt_compat.hpp
+++ ntfmt_compat.hpp
@@ -4,10 +4,8 @@
 #include <stddef.h>
 #include <boost/config.hpp>
 #include <boost/cstdint.hpp>
-#include <boost/integer.hpp>
 
 #if defined(__GNUC__) && !defined(__clang__)
-#include <stdint.h>
 #define ntfmt_likely(x)       __builtin_expect((x),1)
 #define ntfmt_unlikely(x)     __builtin_expect((x),0)
 #else
@@ -20,11 +18,11 @@ namespace ntfmt {
 	namespace detail {
 		typedef ::size_t size_t;
 #if defined(__GNUC__)
-		typedef __UINTPTR_TYPE__ uintptr_t;
+		typedef __typeof__(((int *)0-(int *)0)) uintptr_t;
 #elif defined(_MSC_VER)
 		typedef ::uintptr_t uintptr_t;
 #else
-		typedef typename boost::uint_t<sizeof(void *)>::type uintptr_t;
+		typedef size_t uintptr_t;
 #endif
 	}
 }
